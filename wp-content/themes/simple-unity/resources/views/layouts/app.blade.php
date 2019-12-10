ab982cfd020711915f78d8eef8d9fd2057c468d6
Alisa Herr <alisa@unitymakes.us>
2019-12-10 11:12:05 -0500

    Remove stray GTM code and replace with action for plugin integration

--- wp-content/themes/simple-unity/resources/views/layouts/app.blade.php
+++ wp-content/themes/simple-unity/resources/views/layouts/app.blade.php
@@ -10,12 +10,7 @@
 <html {!! language_attributes() !!} data-text-size="{{ $text_size }}" data-contrast="{{ $contrast }}" class="wf-loading">
   @include('partials.head')
   <body {!! body_class() !!} data-font="{{ $simple_fonts }}" data-color="{{ $simple_color }}" data-buttons="{{ $button_font }}">
-    @if (!is_user_logged_in())
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZPJJVP"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
-    @endif
+    {!! do_action( 'body_open' ) !!}
     <a href="#content" class="screen-reader-text">Skip to content</a>
     <!--[if IE]>
       <div class="alert alert-warning">
