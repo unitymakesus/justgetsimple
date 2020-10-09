351840f29ddbd3d680fe0da427289afd2dd47d89
Andy Marino <andy@unitywebagency.com>
2020-10-08 21:08:55 -0400

    add class for entry-footer

--- wp-content/themes/simple-unity/resources/views/partials/content-single.blade.php
+++ wp-content/themes/simple-unity/resources/views/partials/content-single.blade.php
@@ -6,7 +6,7 @@
   <div class="entry-content">
     @php the_content() @endphp
   </div>
-  <footer>
+  <footer class="entry-footer">
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
     @php comments_template('/partials/comments.blade.php') @endphp
     @include('partials/related-posts')
