2ff15c31656c6b248804bd6f694265285242a053
Andy Marino <andy@unitywebagency.com>
2020-10-20 16:09:51 -0400

    minor style updates to heading and copyright text

--- wp-content/themes/simple-unity/resources/views/partials/related-posts.blade.php
+++ wp-content/themes/simple-unity/resources/views/partials/related-posts.blade.php
@@ -1,6 +1,6 @@
 @if (get_theme_mod('related_posts') && $related_posts->have_posts())
   <section id="related-posts" class="related-posts">
-    <h2>{{ __('Related Posts', 'sage') }}</h2>
+    <h2 class="h3">{{ __('Related Posts', 'sage') }}</h2>
     <div class="related-posts__grid">
       @while ($related_posts->have_posts())
         @php $related_posts->the_post(); @endphp
