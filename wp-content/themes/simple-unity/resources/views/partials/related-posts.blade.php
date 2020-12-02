7e4f83c3e7f4b8a86901abb7771fc5bdb1275901
Andy Marino <andy@unitywebagency.com>
2020-12-02 10:33:18 -0500

    resolve fatal error with related posts partial

--- wp-content/themes/simple-unity/resources/views/partials/related-posts.blade.php
+++ wp-content/themes/simple-unity/resources/views/partials/related-posts.blade.php
@@ -1,4 +1,5 @@
-@if (get_theme_mod('related_posts') && $related_posts->have_posts())
+@if (get_theme_mod('related_posts') && $related_posts)
+  @if ($related_posts->have_posts())
   <section id="related-posts" class="related-posts">
     <h2 class="h3">{{ __('Related Posts', 'sage') }}</h2>
     <div class="related-posts__grid">
@@ -10,4 +11,5 @@
       @endwhile
     </div>
   </section>
+  @endif
 @endif
